2ae95fb8c1263732bdb575fd76d2cb44e038c271
Alexey Kravtsov <akravtsov@spbtv.com>
2019-06-03 01:13:45 +0300

    basic navigation

--- ui/src/main/java/krafts/alex/backupgram/ui/LoginFragment.kt
+++ ui/src/main/java/krafts/alex/backupgram/ui/LoginFragment.kt
@@ -1,22 +1,33 @@
 package krafts.alex.backupgram.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.support.v7.app.AppCompatActivity
+import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import io.reactivex.android.schedulers.AndroidSchedulers
-import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.fragment_login.*
 import krafts.alex.tg.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginFragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
 
+    @SuppressLint("CheckResult")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        setContentView(R.layout.activity_login)
-
-        phone_enter_form.visibility = View.GONE
+        phone_enter_form.visibility = View.VISIBLE
         code_enter_form.visibility = View.GONE
         password_enter_form.visibility = View.GONE
 
@@ -30,26 +41,22 @@ class LoginActivity : AppCompatActivity() {
             code_enter_form.visibility = View.VISIBLE
         }
         TgEvent.listen<AuthOk>().observeOn(AndroidSchedulers.mainThread()).subscribe{
-            Toast.makeText(this, "Already logged in!!", Toast.LENGTH_LONG).show()
+            Snackbar.make(view, "Logged in!", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+            findNavController().navigate(R.id.action_messages)
         }
 
-
-        val client = TgClient(applicationContext)
-
         button_send_phone.setOnClickListener {
-            client.sendPhone(phone.text.toString())
+            BackApp.client.sendPhone(phone.text.toString())
             phone_enter_form.visibility = View.GONE
         }
         button_send_code.setOnClickListener {
-            client.sendCode(code.text.toString())
+            BackApp.client.sendCode(code.text.toString())
             code_enter_form.visibility = View.GONE
         }
         button_send_password.setOnClickListener {
-            client.sendPassword(password.text.toString())
+            BackApp.client.sendPassword(password.text.toString())
             password_enter_form.visibility = View.GONE
         }
-
-        super.onCreate(savedInstanceState)
-
     }
+
 }
--- ui/src/main/java/krafts/alex/backupgram/ui/MainActivity.kt
+++ ui/src/main/java/krafts/alex/backupgram/ui/MainActivity.kt
@@ -1,21 +1,16 @@
 package krafts.alex.backupgram.ui
 
+import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentPagerAdapter
-import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
-
+import androidx.navigation.Navigation
+import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_main.view.*
 import krafts.alex.backupgram.ui.messages.MessagesFragment
+import krafts.alex.tg.AuthOk
+import krafts.alex.tg.TgEvent
 import krafts.alex.tg.entity.Message
 
 class MainActivity : AppCompatActivity(), MessagesFragment.OnListFragmentInteractionListener {
@@ -24,27 +19,28 @@ class MainActivity : AppCompatActivity(), MessagesFragment.OnListFragmentInterac
         //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    /**
-     * The [android.support.v4.view.PagerAdapter] that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * [android.support.v4.app.FragmentStatePagerAdapter].
-     */
-    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
+    var authOk = false
+
+    init {
+        TgEvent.listen<AuthOk>().observeOn(AndroidSchedulers.mainThread()).subscribe {
+            authOk = true
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(toolbar)
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
 
-        // Set up the ViewPager with the sections adapter.
-        container.adapter = mSectionsPagerAdapter
+
+
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        if (!authOk) {
+            navController.navigate(R.id.login_destination)
+        }
+
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -73,58 +69,4 @@ class MainActivity : AppCompatActivity(), MessagesFragment.OnListFragmentInterac
         return super.onOptionsItemSelected(item)
     }
 
-
-    /**
-     * A [FragmentPagerAdapter] that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
-
-        override fun getItem(position: Int): Fragment {
-            if (position == 0){
-                return MessagesFragment()
-            }
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1)
-        }
-
-        override fun getCount(): Int {
-            // Show 3 total pages.
-            return 3
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    class PlaceholderFragment : Fragment() {
-
-        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                                  savedInstanceState: Bundle?): View? {
-            val rootView = inflater.inflate(R.layout.fragment_main, container, false)
-            rootView.section_label.text = getString(R.string.section_format, arguments?.getInt(ARG_SECTION_NUMBER))
-            return rootView
-        }
-
-        companion object {
-            /**
-             * The fragment argument representing the section number for this
-             * fragment.
-             */
-            private val ARG_SECTION_NUMBER = "section_number"
-
-            /**
-             * Returns a new instance of this fragment for the given section
-             * number.
-             */
-            fun newInstance(sectionNumber: Int): PlaceholderFragment {
-                val fragment = PlaceholderFragment()
-                val args = Bundle()
-                args.putInt(ARG_SECTION_NUMBER, sectionNumber)
-                fragment.arguments = args
-                return fragment
-            }
-        }
-    }
 }
